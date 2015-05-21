299f5a292fe54b9f2706dd11fe84f1746c10dab9
Todd Burry <todd@vanillaforums.com>
2015-05-21 09:47:46 -0400

    Change the label sync to use a command. Make the autoloader work with a global require.

--- src/cli.php
+++ src/cli.php
@@ -1,6 +1,5 @@
 <?php
 use Garden\Cli\Cli;
-use Garden\Cli\Schema;
 
 error_reporting(E_ALL); //E_ERROR | E_PARSE | E_CORE_ERROR | E_COMPILE_ERROR | E_USER_ERROR | E_RECOVERABLE_ERROR);
 ini_set('display_errors', 'on');
@@ -8,14 +7,25 @@ ini_set('track_errors', 1);
 
 date_default_timezone_set('America/Montreal');
 
-require_once __DIR__.'/../vendor/autoload.php';
+$paths = [
+    __DIR__.'/../vendor/autoload.php', // locally
+    __DIR__.'/../../../autoload.php' // dependency
+];
+foreach ($paths as $path) {
+    if (file_exists($path)) {
+        require_once $path;
+        break;
+    }
+}
 
 $cli = new Cli();
 
-$cli->description('Copy the labels from one github repo to another.')
-    ->opt('from:f', 'The github repo to copy the labels from.', true)
+$cli->opt('from:f', 'The github repo to copy the labels from.', true)
     ->opt('to:t', 'The github repo to copy the labels to.', true)
     ->opt('token', 'The github access token. Uses the GITHUB_API_TOKEN if not specified.')
+
+    ->command('labels')
+    ->description('Copy the labels from one github repo to another.')
     ->opt('delete:d', 'Whether or not to delete extra labels.', false, 'boolean')
     ;
 
@@ -27,7 +37,11 @@ try {
         ->setFromRepo($args->getOpt('from'))
         ->setToRepo($args->getOpt('to'));
 
-    $sync->syncLabels($args->getOpt('delete'));
+    switch ($args->getCommand()) {
+        case 'labels':
+            $sync->syncLabels($args->getOpt('delete'));
+            break;
+    }
 } catch (Exception $ex) {
     echo $cli->red($ex->getMessage());
     return $ex->getCode();
